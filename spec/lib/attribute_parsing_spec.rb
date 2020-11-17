64044047ac4727817265764827cd90add98e2647
epaew <epaew.333@gmail.com>
2020-11-18 08:53:38 +0900

    tmp

--- spec/lib/attribute_parsing_spec.rb
+++ spec/lib/attribute_parsing_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 class AttributeParsingExampleBase
   include Flexirest::AttributeParsing
 
+  def initialize(attrs={})
+    @attributes = {}
+
+    attrs.each do |attribute_name, attribute_value|
+      attribute_name = attribute_name.to_sym
+      @attributes[attribute_name] = parse_attribute_value(attribute_value)
+    end
+  end
+
+  def method_missing(name, *_args)
+    @attributes[name.to_sym]
+  end
+
+  def respond_to_missing?(method_name, _include_private = false)
+    @attributes.has_key? method_name.to_sym
+  end
+
   def test(v)
     parse_attribute_value(v)
   end
@@ -42,5 +59,18 @@ describe Flexirest::AttributeParsing do
 
   it "should return as a string a date-like string that can't be parsed" do
     expect(subject.test("7/29/2018")).to eq("7/29/2018")
+  end
+
+  it "should return AttributeParsingExampleBase for hash values" do
+    expect(subject.test({ created_at: "1980-12-24" }))
+      .to be_a(AttributeParsingExampleBase)
+      .and have_attributes({ created_at: be_a(Date) })
+  end
+
+  it "should return AttributeParsingExampleBase for hash values in array" do
+    expect(subject.test([{ created_at: "1980-12-24" }]))
+      .to contain_exactly(
+        be_a(AttributeParsingExampleBase).and have_attributes({ created_at: be_a(Date) })
+    )
   end
 end
